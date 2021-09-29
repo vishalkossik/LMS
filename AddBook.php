1b807e76c97060c12bdcccb130d8c010a9ffa75b
Vishal Kossik <luckyvishal2205@gmail.com>
2021-09-29 13:51:23 +0530

    20210929

---
+++ AddBook.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register Student</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <div>
+        <h1>Add Book</h1>
+    </div>
+    <div>
+        <form action="">
+            <label for="">Book Id</label>
+            <input type="text" name="" id="">
+            <label for="">Title</label>
+            <input type="text" name="" id="">
+            <label for="">Author</label>
+            <input type="text" name="" id="">
+            <label>Publication</label>
+            <input type="text" name="" id="">
+            <button>Submit</button>
+        </form>
+    </div>
+</body>
+
+</html>
